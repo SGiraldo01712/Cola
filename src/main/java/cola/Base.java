8381342781c9dae36a873b89df89d8543b35e235
SGiraldo01712 <160081628+SGiraldo01712@users.noreply.github.com>
2024-02-14 19:54:56 -0500

    Initial commit

---
+++ src/main/java/cola/Base.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cola;
+
+/**
+ *
+ * @author sebastian.giraldo_q
+ */
+public abstract class Base {
+    
+    public abstract Base copy();
+}
